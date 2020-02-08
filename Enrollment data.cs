55cd4a1d2ffe5cb798fd051dd617c384ebedb528
elhanshaji <60810977+elhanshaji@users.noreply.github.com>
2020-02-08 15:39:12 +0530

    Add files via upload

---
+++ Enrollment data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado_dot_net_exercise
+{
+    public class Enrollment_data
+    {
+        public string eid;
+        public string sname;
+        public string cname;
+    }
+    public class Snamecomparer:IComparer<Enrollment_data>
+    {
+        public int Compare(Enrollment_data x, Enrollment_data y)
+        {
+            return x.sname.CompareTo(y.sname);
+        }
+    }
+    public class Cnamecomparer : IComparer<Enrollment_data>
+    {
+        public int Compare(Enrollment_data x, Enrollment_data y)
+        {
+            return x.cname.CompareTo(y.cname);
+        }
+    }
+}
